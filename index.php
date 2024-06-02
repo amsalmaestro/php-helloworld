904e74427488f63a435c329d2a482f8c5ad8d14a
Amsal Maestro <amsal.maestro@ti.ukdw.ac.id>
2024-06-02 21:38:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+<title>Hello World!</title>
+</head>
+
+<body>
+<center>
+<?php echo "<h2>Cloud Admin - NodeJS V1</h2>"; ?>
+<?php echo "<p><b>Welcome To OpenShift!</b></p>"; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+
+<?php $links = [];
+  foreach($_ENV as $key => $value) {
+    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
+      $links[] = [
+        "name"  => $matches[1],
+        "port"  => $matches[2],
+        "proto" => $matches[3],
+        "value" => $value
+      ];
+    }
+  }
+
+}
+?>
+  
+</center>
+</body>
+</html>
