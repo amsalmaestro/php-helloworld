6f9bcd03086dd44542ef875cc46867263cdad538
Amsal Maestro <amsal.maestro@ti.ukdw.ac.id>
2024-06-06 23:51:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,24 +7,7 @@
 <body>
 <center>
 <?php echo "<h2>Cloud Admin - BDP IT</h2>"; ?>
-<?php echo "<p><b>Welcome To OpenShift!</b></p>"; ?>
-<?php echo "<h3>My hostname is" <?php echo ["$HOSTNAME"]; ?><br /><br />
-
-<?php $links = [];
-  foreach($_ENV as $key => $value) {
-    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-      $links[] = [
-        "name"  => $matches[1],
-        "port"  => $matches[2],
-        "proto" => $matches[3],
-        "value" => $value
-      ];
-    }
-  }
-
-}
-?>
-  
+<?php echo "<p><b>Welcome To OpenShift!</b></p>"; ?>  
 </center>
 </body>
 </html>
